c3c6ef2f3f518b3773a380c82df95397a4eebf08
Unknown <unknown@example.com>
2019-03-27 08:40:27 +0100

    implementation routing

--- index.js
+++ index.js
@@ -2,23 +2,13 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
+var router = require('./routes/index.js');
 
-//define global users
-users = new Array();
+//define global variables
+global.main_dir = __dirname;
+global.users = new Array();
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
-
-app.get('/login', function(req, res){
-  var username = req.query.username;
-  if(users.includes(username)) {
-    res.sendFile(__dirname + '/index.html');
-  } else {
-    users.push(username);
-    res.sendFile(__dirname + '/chat.html');
-  }
-});
+app.use('/', router);
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
