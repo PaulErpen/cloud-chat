c163cf04e747e8c0993e47913758b5a6bf7491f4
Unknown <unknown@example.com>
2019-03-27 09:27:56 +0100

    less usable

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-var app = require('express')();
+var express = require('express'); 
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 var router = require('./routes/index.js');
+var path = require('path');
+
+//joining paths in order to serve files
+app.use(express.static(path.join(__dirname, 'public')));
 
 //define global variables
 global.main_dir = __dirname;
