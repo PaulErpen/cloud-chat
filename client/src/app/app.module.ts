153b4a6b4e684e32816edc5dfd304cb8704aa7af
Unknown <unknown@example.com>
2019-03-28 16:42:50 +0100

    security works

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,26 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import {Routes, Route, RouterModule } from '@angular/router';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { ChatService } from './chat/services/chat.service';
 import { ChatComponent } from './chat/chat.component';
-import { AuthenticationComponent } from './authentication/authentication.component';
+import { LoginComponent } from './authentication/components/login/login.component';
+import { RegistrationComponent } from './authentication/components/registration/registration.component';
+import { AuthGuard } from './_guards/auth.guard';
 
 const routes : Routes = [
-  {path: '', component:ChatComponent},
-  {path: 'auth', component:AuthenticationComponent}
+  {path: '', component:ChatComponent, canActivate: [AuthGuard]},
+  {path: 'login', component:LoginComponent},
+  {path: 'registration', component:RegistrationComponent}
 ]
 @NgModule({
   declarations: [
     AppComponent,
-    AuthenticationComponent,
-    ChatComponent
+    ChatComponent,
+    LoginComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    HttpClientModule
     ],
   providers: [ChatService],
   bootstrap: [AppComponent]
