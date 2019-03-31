86897705fed10b79bb2c10ee5d9a544ed13964c8
Unknown <unknown@example.com>
2019-03-31 13:37:24 +0200

    login working again

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 var express = require('express'); 
 var app = express();
 var http = require('http').Server(app);
-var io = require('socket.io')(http);
+global.io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 var path = require('path');
 var router = require('./routes/router.js');
--- server/user/user.js
+++ server/user/user.js
@@ -3,7 +3,7 @@ const express = require('express');
 function login (username, password) {
     var user = users.filter(u => u.username == username && u.password == password);
     if(user.length > 0) {
-        online_user.adapter({"username" : username, "password" : password});
+        //online_user.adapter({"username" : username, "password" : password});
         online_user.push({"username" : "ONLINEUSER"});
         // online_user.push({"username":"Test3"});
         io.emit('online users', online_user);
