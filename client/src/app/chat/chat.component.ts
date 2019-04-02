312e333ff6b09ebddcf24ffcdbe2b9b57d14089d
Unknown <unknown@example.com>
2019-04-02 16:02:39 +0200

    not working with socket io

--- client/src/app/chat/chat.component.ts
+++ client/src/app/chat/chat.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ChatService } from './services/chat.service';
+import  *  as $ from 'jquery';
 
 @Component({
   selector: 'app-chat',
@@ -9,12 +10,24 @@ import { ChatService } from './services/chat.service';
 export class ChatComponent implements OnInit {
   message: string;  
   messages: string[] = [];
+  files: FileList;
 
   constructor(private chatService: ChatService) { }
 
   sendMessage() {
+    if(this.files != undefined) {
+      this.chatService.sendFiles(this.files);
+      this.files = undefined;
+      $('#uploadfiles').val("");
+    }
     this.chatService.sendMessage(this.message);
     this.message = '';
+  }
+
+  onFileChange(event) { 
+    if(event.target.files && event.target.files.length) {
+      this.files = event.target.files;
+    }
   }
 
   ngOnInit() {
