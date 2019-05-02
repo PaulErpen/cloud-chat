ac15d342a99b0e688223ad8feaab1bad33bc63b4
Paul Erpenstein <paul.erpenstein@gmail.com>
2019-05-02 09:45:01 +0200

    removal of unnecessary async class functions

--- database/authentication.js
+++ database/authentication.js
@@ -1,110 +1,112 @@
 const request = require('request-promise');
 const database = require('./database');
 
-class Authentication {
-    /**
-     * Returns User if it exists in users, else returns false
-     * @param username
-     * @param password
-     * @returns {*}
-     */
-    async login (username, password) {
-        return database.getToken().then((token) => {
-            var options = {
-                method: 'POST',
-                url: process.env.DATABASE_URL+'/sql_jobs',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': "Bearer " + token
-                },
-                body: {
-                    "commands": "SELECT username, password FROM users u WHERE u.password = '''"+password+"''' AND u.username ='''"+username+"'''",
-                    "limit": 10,
-                    "separator": ";",
-                    "stop_on_error": "no"
-                },
-                json: true
-            };
-    
-            return request(options).then(
-                (body) => {
-                    return database.awaitQuery(body);
-                }
-            ).catch(function(error) {
-                throw error;
-            });
-        });
-    }
+/**
+ * Returns User if it exists in users, else returns false
+ * @param username
+ * @param password
+ * @returns {*}
+ */
+async function login (username, password) {
+    return database.getToken().then((token) => {
+        var options = {
+            method: 'POST',
+            url: process.env.DATABASE_URL+'/sql_jobs',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': "Bearer " + token
+            },
+            body: {
+                "commands": "SELECT username, password FROM users u WHERE u.password = '''"+password+"''' AND u.username ='''"+username+"'''",
+                "limit": 10,
+                "separator": ";",
+                "stop_on_error": "no"
+            },
+            json: true
+        };
 
-    /**
-     * Tests if user exists in user
-     * true: returns false
-     * false: adds new user to user and returns user
-     * @param username
-     * @param password
-     * @returns {*}
-     */
-    async register (username, password, userpic) {
-        return this.userExists(username).then(
-            (result) => {
-                if(result[0].rows_count == 0) {
-                    return database.getToken().then((token) => {
-                        var options = {
-                            method: 'POST',
-                            url: process.env.DATABASE_URL+'/sql_jobs',
-                            headers: {
-                                'Content-Type': 'application/json',
-                                'Authorization': "Bearer " + token
-                            },
-                            body: {
-                                "commands": "INSERT INTO users VALUES('''" + username + "''', '''"+password+"''', '''"+userpic+"''')",
-                                "limit": 10,
-                                "separator": ";",
-                                "stop_on_error": "no"
-                            },
-                            json: true
-                        };
-                
-                        return request(options).then(
-                            (body) => {
-                                return database.awaitQuery(body);
-                            }
-                        ).catch(function(error) {
-                            throw error;
-                        });
-                    });
-                }
-                return false;
+        return request(options).then(
+            (body) => {
+                return database.awaitQuery(body);
             }
-        );
-    }
-
-    userExists(username) {
-        return database.getToken().then((token) => {
-            var options = {
-                method: 'POST',
-                url: process.env.DATABASE_URL+'/sql_jobs',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': "Bearer " + token
-                },
-                body: {
-                    "commands": "SELECT username, password FROM users u WHERE u.username ='''"+username+"'''",
-                    "limit": 10,
-                    "separator": ";",
-                    "stop_on_error": "no"
-                },
-                json: true
-            };
-    
-            return request(options).then(
-                (body) => {
-                    return database.awaitQuery(body);
-                }
-            ).catch(function(error) {
-                throw error;
-            });
+        ).catch(function(error) {
+            throw error;
         });
-    }
+    });
 }
-module.exports = Authentication;
+
+/**
+ * Tests if user exists in user
+ * true: returns false
+ * false: adds new user to user and returns user
+ * @param username
+ * @param password
+ * @returns {*}
+ */
+async function register (username, password, userpic) {
+    return userExists(username).then(
+        (result) => {
+            if(result[0].rows_count == 0) {
+                return database.getToken().then((token) => {
+                    var options = {
+                        method: 'POST',
+                        url: process.env.DATABASE_URL+'/sql_jobs',
+                        headers: {
+                            'Content-Type': 'application/json',
+                            'Authorization': "Bearer " + token
+                        },
+                        body: {
+                            "commands": "INSERT INTO users VALUES('''" + username + "''', '''"+password+"''', '''"+userpic+"''')",
+                            "limit": 10,
+                            "separator": ";",
+                            "stop_on_error": "no"
+                        },
+                        json: true
+                    };
+            
+                    return request(options).then(
+                        (body) => {
+                            return database.awaitQuery(body);
+                        }
+                    ).catch(function(error) {
+                        throw error;
+                    });
+                });
+            }
+            return false;
+        }
+    );
+}
+
+function userExists(username) {
+    return database.getToken().then((token) => {
+        var options = {
+            method: 'POST',
+            url: process.env.DATABASE_URL+'/sql_jobs',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': "Bearer " + token
+            },
+            body: {
+                "commands": "SELECT username, password FROM users u WHERE u.username ='''"+username+"'''",
+                "limit": 10,
+                "separator": ";",
+                "stop_on_error": "no"
+            },
+            json: true
+        };
+
+        return request(options).then(
+            (body) => {
+                return database.awaitQuery(body);
+            }
+        ).catch(function(error) {
+            throw error;
+        });
+    });
+}
+module.exports = {
+    "userExists": userExists,
+    "register": register,
+    "login": login
+};
--- routes/router.js
+++ routes/router.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const authentication = new require('../database/authentication');
 const userimage = new require('../database/userimage');
-const auth = new authentication();
+const auth = new require('../database/authentication');
 const database = require("../database/database");
 
 /**
