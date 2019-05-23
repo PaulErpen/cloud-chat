b5de540b2a145122756c28a912a0ea7e8a111599
Paul Erpenstein <paul.erpenstein@gmail.com>
2019-05-23 15:43:29 +0200

    more changes

--- index.js
+++ index.js
@@ -68,48 +68,54 @@ app.use('/', router);
 
 /*configuration of MQ on IBM Cloud in order
 to make inter-service communication possible*/
-var mq_options = {};
-if(process.env.VCAP_SERVICES) {
+var opts = {}
+var mqlightService = {}
+if (process.env.VCAP_SERVICES) {
   var services = JSON.parse(process.env.VCAP_SERVICES);
   console.log('Running BlueMix');
   for (var key in services) {
     if (key.lastIndexOf(mqlightServiceName, 0) === 0) {
       mqlightService = services[key][0];
-      mq_options.service = mqlightService.credentials.nonTLSConnectionLookupURI;
-      mq_options.user = mqlightService.credentials.username;
-      mq_options.password = mqlightService.credentials.password;
+      opts.service = mqlightService.credentials.nonTLSConnectionLookupURI;
+      opts.user = mqlightService.credentials.username;
+      opts.password = mqlightService.credentials.password;
     } else if (key.lastIndexOf(messageHubServiceName, 0) === 0) {
       messageHubService = services[key][0];
-      mq_options.service = messageHubService.credentials.mqlight_lookup_url;
-      mq_options.user = messageHubService.credentials.user;
-      mq_options.password = messageHubService.credentials.password;
+      opts.service = messageHubService.credentials.mqlight_lookup_url;
+      opts.user = messageHubService.credentials.user;
+      opts.password = messageHubService.credentials.password;
     }
   }
-  if (!mq_options.hasOwnProperty('service') ||
-      !mq_options.hasOwnProperty('user') ||
-      !mq_options.hasOwnProperty('password')) {
+  if (!opts.hasOwnProperty('service') ||
+      !opts.hasOwnProperty('user') ||
+      !opts.hasOwnProperty('password')) {
     throw 'Error - Check that app is bound to service';
   }
+} else if (process.env.mqlight_lookup_url &&
+           process.env.mqlight_user &&
+           process.env.mqlight_password) {
+  opts.service = process.env.mqlight_lookup_url;
+  opts.user = process.env.mqlight_user;
+  opts.password = process.env.mqlight_password;
 } else {
-  mq_options.service = process.env.MQ_LIGHT_SERVICE;
-  mq_options.user = process.env.MQ_LIGHT_USER;
-  mq_options.password = process.env.MQ_LIGHT_PASSWORD;
+  var fishaliveHost = process.env.FISHALIVE_HOST || 'localhost'
+  opts.service = 'amqp://' + "localhost" + ':5672'
 }
-var sendClient = mqlight.createClient(mq_options, function (err) {
+opts.id = 'NODE_WORKER_' + uuid.v4().substring(0, 7)
+
+/*
+ * Create our MQ Light client
+ * If we are not running in Bluemix, then default to a local MQ Light
+ * connection
+ */
+var mqlightClient = mqlight.createClient(opts, function (err) {
   if (err) {
-    console.error('Connection to ' + mq_options.service + ' using client-id ' +
-    sendClient.id + ' failed: ' + err)
+    console.error('Connection to ' + opts.service + ' using client-id ' +
+      mqlightClient.id + ' failed: ' + err)
   } else {
-    console.log('Connected to ' + mq_options.service + ' using client-id ' +
-    sendClient.id)
+    console.log('Connected to ' + opts.service + ' using client-id ' +
+      mqlightClient.id)
   }
-});
-var topic = '';
-sendClient.on('started', function() {
-  sendClient.send(topic, 'Hello World!', function (err, data) {
-    console.log('Sent: %s', data);
-    sendClient.stop();
-  });
 });
 
 //define global variables, which will be used in other modules
