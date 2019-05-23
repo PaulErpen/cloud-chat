c256784b152d73fc65822b6a80107a59e3373a40
Paul Erpenstein <paul.erpenstein@gmail.com>
2019-05-23 15:48:38 +0200

    reset of all mq changes

--- index.js
+++ index.js
@@ -17,7 +17,6 @@ var messages = require('./messages/messages');
 var cors = require("cors");
 var xFrameOptions = require('x-frame-options')
 const hsts = require('hsts');
-var mqlight = require('mqlight');
 
 //SAFETY CONFIG START
 if(node_env != 'development') {
@@ -66,57 +65,7 @@ app.use(function (req, res, next) {
 app.use('/', router);
 //REQUEST CONFIG END
 
-/*configuration of MQ on IBM Cloud in order
-to make inter-service communication possible*/
-var opts = {}
-var mqlightService = {}
-if (process.env.VCAP_SERVICES) {
-  var services = JSON.parse(process.env.VCAP_SERVICES);
-  console.log('Running BlueMix');
-  for (var key in services) {
-    if (key.lastIndexOf(mqlightServiceName, 0) === 0) {
-      mqlightService = services[key][0];
-      opts.service = mqlightService.credentials.nonTLSConnectionLookupURI;
-      opts.user = mqlightService.credentials.username;
-      opts.password = mqlightService.credentials.password;
-    } else if (key.lastIndexOf(messageHubServiceName, 0) === 0) {
-      messageHubService = services[key][0];
-      opts.service = messageHubService.credentials.mqlight_lookup_url;
-      opts.user = messageHubService.credentials.user;
-      opts.password = messageHubService.credentials.password;
-    }
-  }
-  if (!opts.hasOwnProperty('service') ||
-      !opts.hasOwnProperty('user') ||
-      !opts.hasOwnProperty('password')) {
-    throw 'Error - Check that app is bound to service';
-  }
-} else if (process.env.mqlight_lookup_url &&
-           process.env.mqlight_user &&
-           process.env.mqlight_password) {
-  opts.service = process.env.mqlight_lookup_url;
-  opts.user = process.env.mqlight_user;
-  opts.password = process.env.mqlight_password;
-} else {
-  var fishaliveHost = process.env.FISHALIVE_HOST || 'localhost'
-  opts.service = 'amqp://' + "localhost" + ':5672'
-}
-opts.id = 'NODE_WORKER_' + uuid.v4().substring(0, 7)
 
-/*
- * Create our MQ Light client
- * If we are not running in Bluemix, then default to a local MQ Light
- * connection
- */
-var mqlightClient = mqlight.createClient(opts, function (err) {
-  if (err) {
-    console.error('Connection to ' + opts.service + ' using client-id ' +
-      mqlightClient.id + ' failed: ' + err)
-  } else {
-    console.log('Connected to ' + opts.service + ' using client-id ' +
-      mqlightClient.id)
-  }
-});
 
 //define global variables, which will be used in other modules
 global.main_dir = __dirname;
