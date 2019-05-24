707ff632482ab540060ba463b5f16cb071b725e3
Paul Erpenstein <paul.erpenstein@gmail.com>
2019-05-24 17:07:44 +0200

    removal of example message consumer and producer

--- messagebroker/messagebroker.js
+++ messagebroker/messagebroker.js
@@ -126,29 +126,6 @@ function handleMessageData(message) {
   }
 }
 
-// Publisher
-open.then(function(conn) {
-  return conn.createChannel();
-}).then(function(ch) {
-  return ch.assertQueue(q).then(function(ok) {
-    return ch.sendToQueue(q, Buffer.from('something to do'));
-  });
-}).catch(console.warn);
-
-// Consumer
-// open.then(function(conn) {
-//   return conn.createChannel();
-// }).then(function(ch) {
-//   return ch.assertQueue(q).then(function(ok) {
-//     return ch.consume(q, function(msg) {
-//       if (msg !== null) {
-//         console.log(msg.content.toString());
-//         ch.ack(msg);
-//       }
-//     });
-//   });
-// }).catch(console.warn);
-
 module.exports = {
   "setupQueueListener": setupQueueListener,
   "notifyUserLogin": notifyUserLogin,
